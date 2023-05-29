ad8caa9545d75739e6265956ee604ee88dd39d9d
jones <35585020+fleeced@users.noreply.github.com>
2023-05-29 12:01:21 -0600

    added error handling to contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,54 +1,143 @@
 <template>
-    <footer ref="footer" class="footer">
-        <p>
-            Is your league interested in using Ref Buddy to assign games and
-            track stats? Feel free to reach out with any questions and we will
-            get back to you right away!
-        </p>
-        <form ref="footerForm" class="footer__form">
-            <input v-model="form.Name" type="text" placeholder="Name" />
-            <input v-model="form.Email" type="email" placeholder="Email" />
-            <input v-model="form.League" type="text" placeholder="League" />
-            <textarea v-model="form.Message" placeholder="Message"></textarea>
-            <button @click.prevent="submitForm">Submit</button>
-        </form>
-    </footer>
-    <div v-if="success" class="footer__success">
-        We have received your message and will get back to you as soon as
-        possible!
-    </div>
+  <footer ref="footer" class="footer">
+    <p>
+      Is your league interested in using Ref Buddy to assign games and track stats? Feel free to reach out with any questions and we will get back to you right away!
+    </p>
+    <form ref="footerForm" class="footer__form">
+      <div class="form-group">
+        <div class="input-wrapper">
+          <input v-model="form.Name" type="text" placeholder="Name" />
+          <div v-if="errors.Name" class="error">{{ errors.Name }}</div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="input-wrapper">
+          <input v-model="form.Email" type="email" placeholder="Email" />
+          <div v-if="errors.Email" class="error">{{ errors.Email }}</div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="input-wrapper">
+          <input v-model="form.League" type="text" placeholder="League" />
+          <div v-if="errors.League" class="error">{{ errors.League }}</div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="input-wrapper">
+          <textarea v-model="form.Message" placeholder="Message"></textarea>
+          <div v-if="errors.Message" class="error">{{ errors.Message }}</div>
+        </div>
+      </div>
+      <button @click.prevent="submitForm">Submit</button>
+    </form>
+  </footer>
+  <div v-if="success" class="footer__success">
+    We have received your message and will get back to you as soon as possible!
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    data() {
-        return {
-            form: {
-                Name: "",
-                Email: "",
-                League: "",
-                Message: "",
-            },
-            success: false,
-        };
+  data() {
+    return {
+      form: {
+        Name: "",
+        Email: "",
+        League: "",
+        Message: "",
+      },
+      errors: {},
+      success: false,
+    };
+  },
+  methods: {
+    async submitForm() {
+      // Clear any existing error messages
+      this.errors = {};
+
+      // Check that all fields have a value
+      if (!this.form.Name) {
+        this.errors.Name = "Please enter your name.";
+      }
+      if (!this.form.Email) {
+        this.errors.Email = "Please enter your email address.";
+      }
+      if (!this.form.League) {
+        this.errors.League = "Please enter your league's name.";
+      }
+      if (!this.form.Message) {
+        this.errors.Message = "Please enter a message.";
+      }
+      if (Object.keys(this.errors).length > 0) {
+        return;
+      }
+
+      // Check that the email field has a valid email address
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(this.form.Email)) {
+        this.errors.Email = "Please enter a valid email address.";
+        return;
+      }
+
+      try {
+        await axios.post(
+          "https://us-central1-ref-buddy-d7be3.cloudfunctions.net/sendContactForm",
+          { data: this.form }
+        );
+        this.success = true;
+        this.$refs.footerForm.style.display = "none";
+        this.$refs.footer.style.display = "none";
+      } catch (error) {
+        console.error(error);
+        alert("An error occurred while submitting the form.");
+      }
     },
-    methods: {
-        async submitForm() {
-            try {
-                await axios.post(
-                    "https://us-central1-ref-buddy-d7be3.cloudfunctions.net/sendContactForm",
-                    { data: this.form }
-                );
-                this.success = true;
-                this.$refs.footerForm.style.display = "none";
-                this.$refs.footer.style.display = "none";
-            } catch (error) {
-                console.error(error);
-                alert("An error occurred while submitting the form.");
-            }
-        },
-    },
+  },
 };
 </script>
+
+<style>
+    .form-group {
+        margin-bottom: 20px;
+    }
+
+    .input-wrapper {
+        display: flex;
+        flex-direction: column;
+    }
+
+    input[type="text"],
+    input[type="email"],
+    textarea {
+        display: block;
+        width: 95%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        font-size: 16px;
+        margin-bottom: 10px;
+        outline-color: rgba(255, 159, 33, 0.73);
+    }
+
+    textarea {
+        height: 150px;
+    }
+
+    .error {
+        color: red;
+        font-size: 14px;
+        margin-top: -4px;
+        margin-bottom: -12px;
+    }
+
+    .footer__success {
+        background-color: #dff0d8;
+        color: #000000;
+        border: 1px solid #000000;
+        border-radius: 5px;
+        padding: 20px;
+        margin-top: 20px;
+    }
+</style>
