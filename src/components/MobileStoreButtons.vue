8a89691c573a479c630911b113512973511f10a1
jones <35585020+fleeced@users.noreply.github.com>
2023-05-29 12:48:31 -0600

    store links open in new tab

--- src/components/MobileStoreButtons.vue
+++ src/components/MobileStoreButtons.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="store-buttons">
-    <a href="https://apps.apple.com/ca/app/ref-buddy/id6444165901">
+    <a href="https://apps.apple.com/ca/app/ref-buddy/id6444165901" target="_blank">
       <img src="@/assets/app-store-badge.png" alt="Available on the App Store" />
     </a>
-    <a href="https://play.google.com/store/apps/details?id=com.duckwxrth.refbuddy">
+    <a href="https://play.google.com/store/apps/details?id=com.duckwxrth.refbuddy"  target="_blank">
       <img src="@/assets/google-play-badge.png" alt="Available on Google Play" />
     </a>
   </div>
